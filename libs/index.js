d463ba202b11c1247c03612a912eebc60cd5e4c6
Leo Cheung <leozhang621@gmail.com>
2023-11-23 17:51:42 +0800

    feat: add 3d base

--- libs/index.js
+++ libs/index.js
@@ -19,10 +19,7 @@ function initShader(gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE) {
 
 function getTranslateMatrix(x = 0, y = 0, z = 0) {
   return new Float32Array([
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0,
-    x, y, z, 1.0
+    1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, x, y, z, 1.0
   ])
 }
 
@@ -30,19 +27,13 @@ function getRotateZMatrix(deg) {
   const cos = Math.cos
   const sin = Math.sin
   return new Float32Array([
-    cos(deg), sin(deg), 0.0, 0.0,
-    -sin(deg), cos(deg), 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0
+    cos(deg), sin(deg), 0.0, 0.0, -sin(deg), cos(deg), 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0
   ])
 }
 
 function getScaleMatrix(x = 1, y = 1, z = 1) {
   return new Float32Array([
-    x, 0.0, 0.0, 0.0,
-    0.0, y, 0.0, 0.0,
-    0.0, 0.0, z, 0.0,
-    0.0, 0.0, 0.0, 1.0
+    x, 0.0, 0.0, 0.0, 0.0, y, 0.0, 0.0, 0.0, 0.0, z, 0.0, 0.0, 0.0, 0.0, 1.0
   ])
 }
 
@@ -55,4 +46,49 @@ function mixMatrix(a, b) {
     result[i + 12] = a[i] * b[12] + a[i + 4] * b[13] + a[i + 8] * b[14] + a[i + 12] * b[15]
   }
   return result
+}
+
+// 归一化
+function normalized(arr) {
+  const sum = arr.reduce((prev, cur) => prev + cur * cur, 0)
+  const middle = Math.sqrt(sum)
+  return arr.map(item => item / middle)
+}
+
+// 向量叉积，获取法向量
+function cross(a, b) {
+  return new Float32Array([
+    a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0]
+  ])
+}
+
+// 点积，求投影长度
+function dot(a, b) {
+  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+}
+
+// 向量差
+function minus(a, b) {
+  return new Float32Array([
+    a[0] - b[0], a[1] - b[1], a[2] - b[2]
+  ])
+}
+
+// 获取视图矩阵
+function getViewMatrix(eyex,eyey,eyez,lookAtx,lookAty,lookAtz,upx,upy,upz) {
+  const eye = new Float32Array([eyex,eyey,eyez])
+  const lookAt = new Float32Array([lookAtx,lookAty,lookAtz])
+  const up = new Float32Array([upx,upy,upz])
+  const z = minus(eye, lookAt)
+  normalized(z)
+  normalized(up)
+  const x = cross(z, up)
+  normalized(x)
+  const y = cross(x, z)
+  return new Float32Array([
+    x[0], y[0], z[0], 0.0,
+    x[1], y[1], z[1], 0.0,
+    x[2], y[2], z[2], 0.0,
+    -dot(x, eye), -dot(y, eye), -dot(z, eye), 1.0
+  ])
 }
